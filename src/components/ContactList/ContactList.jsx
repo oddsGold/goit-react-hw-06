c2a45e9d0790aa7acecbe41110399c2e41e11447
Sulym Dmytro <ds.intelserv@gmail.com>
2024-04-29 21:51:38 +0300

    Fixed filters

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import contactList  from "./ContactList.module.css"
 import Contact from "../Contact/Contact.jsx";
 import {useSelector} from "react-redux";
-import {selectContacts} from "../../redux/selectors.js";
+import {selectContacts, selectNameFilter} from "../../redux/selectors.js";
 
 const ContactList = () => {
+
+    const name = useSelector(selectNameFilter);
     const contacts = useSelector(selectContacts);
+
+    useEffect(() => {
+        const filteredContacts = contacts.filter(contact =>
+            contact.name.toLowerCase().includes(name.toLowerCase())
+        );
+        setArrayFiltered(filteredContacts);
+    }, [name, contacts]);
+
+    const [arrayFiltered, setArrayFiltered] = React.useState([]);
 
     return (
         <div className={contactList["contact-list"]}>
             {
-                contacts.map((item) => {
+                arrayFiltered.map((item) => {
                     return(
                         <div key={item.id} className={contactList["contact-list-item"]}>
                             <Contact
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import search from "./SearchBox.module.css"
 import {useDispatch, useSelector} from "react-redux";
-import {selectNameFilter} from "../../redux/selectors.js";
 import {changeFilter} from "../../redux/filtersSlice.js";
 
 const SearchBox = () => {
     const dispatch = useDispatch();
-    const name = useSelector(selectNameFilter);
 
     return(
         <div className={search["handleSearch"]}>
@@ -15,7 +13,6 @@ const SearchBox = () => {
                 type="text"
                 onChange={(e) => dispatch(changeFilter(e.target.value))}
             />
-            <p className={search["res-filter-name"]}>{name}</p>
         </div>
     )
 }
